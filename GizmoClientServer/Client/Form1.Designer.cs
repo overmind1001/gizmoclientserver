67ec31a8234cc6af0d8877ddbee9874b169c4aef
overmind1001 <overmind1001@277a856a-4f4b-458e-875c-85b963333f67>
2011-10-20 16:15:26 +0000

    Запилил Ололо!

--- GizmoClientServer/Client/Form1.Designer.cs
+++ GizmoClientServer/Client/Form1.Designer.cs
@@ -39,6 +39,7 @@
             this.label1.Size = new System.Drawing.Size(61, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "ТРОЛОЛО";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
