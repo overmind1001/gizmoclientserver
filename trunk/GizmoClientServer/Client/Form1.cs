c951930c7e5182202b0d4652706ed3f77cbd16f7
overmind1001 <overmind1001@277a856a-4f4b-458e-875c-85b963333f67>
2011-10-20 16:15:26 +0000

    Запилил Ололо!

--- trunk/GizmoClientServer/Client/Form1.cs
+++ trunk/GizmoClientServer/Client/Form1.cs
@@ -15,5 +15,10 @@ namespace Client
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ололо");
+        }
     }
 }
