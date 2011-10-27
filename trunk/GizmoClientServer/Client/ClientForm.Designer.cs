fbfa0b57e1a66924e67a6e278099c54f33f721aa
overmind1001 <overmind1001@277a856a-4f4b-458e-875c-85b963333f67>
2011-10-27 10:58:08 +0000

    Сделал интерфейс для основной формы клиента. Там значит чат и доступ к файлам.

---
+++ trunk/GizmoClientServer/Client/ClientForm.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace Client
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageChat = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBoxChat = new System.Windows.Forms.GroupBox();
+            this.tbChat = new System.Windows.Forms.TextBox();
+            this.groupBoxPeople = new System.Windows.Forms.GroupBox();
+            this.lbPeople = new System.Windows.Forms.ListBox();
+            this.tabPageFileStorage = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.lbFilesList = new System.Windows.Forms.ListBox();
+            this.menuStripMainMenu = new System.Windows.Forms.MenuStrip();
+            this.главнаяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1.SuspendLayout();
+            this.tabPageChat.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBoxChat.SuspendLayout();
+            this.groupBoxPeople.SuspendLayout();
+            this.tabPageFileStorage.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.menuStripMainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageChat);
+            this.tabControl1.Controls.Add(this.tabPageFileStorage);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.HotTrack = true;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(704, 426);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPageChat
+            // 
+            this.tabPageChat.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPageChat.Controls.Add(this.tableLayoutPanel1);
+            this.tabPageChat.Location = new System.Drawing.Point(4, 22);
+            this.tabPageChat.Name = "tabPageChat";
+            this.tabPageChat.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageChat.Size = new System.Drawing.Size(696, 400);
+            this.tabPageChat.TabIndex = 0;
+            this.tabPageChat.Text = "Чат";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.splitContainer1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(690, 394);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSend);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.tbMessage);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 337);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(684, 54);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(588, 28);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(89, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Отправить!";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Написать сообщение:";
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbMessage.Location = new System.Drawing.Point(13, 28);
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(569, 20);
+            this.tbMessage.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBoxChat);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBoxPeople);
+            this.splitContainer1.Size = new System.Drawing.Size(684, 328);
+            this.splitContainer1.SplitterDistance = 499;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // groupBoxChat
+            // 
+            this.groupBoxChat.Controls.Add(this.tbChat);
+            this.groupBoxChat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxChat.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxChat.Name = "groupBoxChat";
+            this.groupBoxChat.Size = new System.Drawing.Size(499, 328);
+            this.groupBoxChat.TabIndex = 1;
+            this.groupBoxChat.TabStop = false;
+            this.groupBoxChat.Text = "Чат";
+            // 
+            // tbChat
+            // 
+            this.tbChat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbChat.Location = new System.Drawing.Point(3, 16);
+            this.tbChat.Multiline = true;
+            this.tbChat.Name = "tbChat";
+            this.tbChat.Size = new System.Drawing.Size(493, 309);
+            this.tbChat.TabIndex = 0;
+            // 
+            // groupBoxPeople
+            // 
+            this.groupBoxPeople.Controls.Add(this.lbPeople);
+            this.groupBoxPeople.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxPeople.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxPeople.Name = "groupBoxPeople";
+            this.groupBoxPeople.Size = new System.Drawing.Size(181, 328);
+            this.groupBoxPeople.TabIndex = 3;
+            this.groupBoxPeople.TabStop = false;
+            this.groupBoxPeople.Text = "Люди";
+            // 
+            // lbPeople
+            // 
+            this.lbPeople.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbPeople.FormattingEnabled = true;
+            this.lbPeople.Location = new System.Drawing.Point(3, 16);
+            this.lbPeople.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.lbPeople.Name = "lbPeople";
+            this.lbPeople.Size = new System.Drawing.Size(175, 309);
+            this.lbPeople.TabIndex = 2;
+            // 
+            // tabPageFileStorage
+            // 
+            this.tabPageFileStorage.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPageFileStorage.Controls.Add(this.tableLayoutPanel2);
+            this.tabPageFileStorage.Location = new System.Drawing.Point(4, 22);
+            this.tabPageFileStorage.Name = "tabPageFileStorage";
+            this.tabPageFileStorage.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageFileStorage.Size = new System.Drawing.Size(696, 400);
+            this.tabPageFileStorage.TabIndex = 1;
+            this.tabPageFileStorage.Text = "Файлы";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.lbFilesList, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 57F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(690, 394);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnDownload);
+            this.panel2.Controls.Add(this.btnUpload);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 340);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(684, 51);
+            this.panel2.TabIndex = 0;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDownload.Location = new System.Drawing.Point(390, 10);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(121, 23);
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Text = "Скачать файл";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpload.Location = new System.Drawing.Point(533, 10);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(132, 23);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Загрузить на сервер...";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            // 
+            // lbFilesList
+            // 
+            this.lbFilesList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbFilesList.FormattingEnabled = true;
+            this.lbFilesList.Location = new System.Drawing.Point(3, 3);
+            this.lbFilesList.Name = "lbFilesList";
+            this.lbFilesList.Size = new System.Drawing.Size(684, 331);
+            this.lbFilesList.TabIndex = 1;
+            // 
+            // menuStripMainMenu
+            // 
+            this.menuStripMainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.главнаяToolStripMenuItem});
+            this.menuStripMainMenu.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMainMenu.Name = "menuStripMainMenu";
+            this.menuStripMainMenu.Size = new System.Drawing.Size(704, 24);
+            this.menuStripMainMenu.TabIndex = 1;
+            this.menuStripMainMenu.Text = "menuStrip1";
+            // 
+            // главнаяToolStripMenuItem
+            // 
+            this.главнаяToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem});
+            this.главнаяToolStripMenuItem.Name = "главнаяToolStripMenuItem";
+            this.главнаяToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.главнаяToolStripMenuItem.Text = "Главная";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStripMainMenu);
+            this.MainMenuStrip = this.menuStripMainMenu;
+            this.Name = "ClientForm";
+            this.Text = "Клиент";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageChat.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBoxChat.ResumeLayout(false);
+            this.groupBoxChat.PerformLayout();
+            this.groupBoxPeople.ResumeLayout(false);
+            this.tabPageFileStorage.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.menuStripMainMenu.ResumeLayout(false);
+            this.menuStripMainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageChat;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox tbChat;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.ListBox lbPeople;
+        private System.Windows.Forms.TabPage tabPageFileStorage;
+        private System.Windows.Forms.MenuStrip menuStripMainMenu;
+        private System.Windows.Forms.ToolStripMenuItem главнаяToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBoxChat;
+        private System.Windows.Forms.GroupBox groupBoxPeople;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.ListBox lbFilesList;
+
+    }
+}
+
