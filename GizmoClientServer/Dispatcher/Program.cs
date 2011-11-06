581fb733bf9ae9d00669ab79c325a0e5fc85b5ef
overmind1001 <overmind1001@277a856a-4f4b-458e-875c-85b963333f67>
2011-11-06 10:25:48 +0000

    Dispatcher. Переименовал форму.))

--- GizmoClientServer/Dispatcher/Program.cs
+++ GizmoClientServer/Dispatcher/Program.cs
@@ -15,7 +15,7 @@ namespace Dispatcher
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DispatcherForm());
         }
     }
 }
