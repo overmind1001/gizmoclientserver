7f760ed0f2d168c452d6087537900b11a466b9f0
San4oKiselev@gmail.com <San4oKiselev@gmail.com@277a856a-4f4b-458e-875c-85b963333f67>
2011-11-07 17:46:47 +0000

    Чуть больше

--- trunk/GizmoClientServer/Server/ClientItem.cs
+++ trunk/GizmoClientServer/Server/ClientItem.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System.IO.Pipes;
 
 namespace MsgServer
 {
+    /// <summary>
+    /// Состояние клиента
+    /// </summary>
+    enum ClientState
+    {
+        Connected,
+        Disconnected
+    };
+
     /// <summary>
     /// Класс клиента
     /// </summary>
@@ -13,18 +25,76 @@ namespace MsgServer
     {
         private string m_Name;
         private TcpClient m_Tcp;
+        private Thread m_ServeThread;
+        private ClientState m_State;
 
-        // Паблики
-
-        public string name
+        public ClientItem(TcpClient tcp, string name)
         {
-            get { return m_Name; }
-            set { m_Name = value; }
+            m_Tcp = tcp;
+            m_Name = name;
+            m_State = ClientState.Connected;
         }
 
-        // В потоке
-        public void Serve()
+        ~ClientItem()
         {
+            m_ServeThread.Abort();
+        }
+
+        /// <summary>
+        /// Посылаем другим клиентам текстовое сообщение
+        /// </summary>
+        /// <param name="text"></param>
+        private void SendTextToAll(string text)
+        {
+        }
+
+        /// <summary>
+        /// Начинаем обслуживание клиента
+        /// </summary>
+        public void StartServe()
+        {
+            m_ServeThread = new Thread(ServeThreadFunc);
+            m_ServeThread.Start();
+        }
+
+        /// <summary>
+        /// Поток обслуживания
+        /// </summary>
+        public void ServeThreadFunc()
+        {
+            StreamReader reader = new StreamReader(m_Tcp.GetStream());
+            string line;
+            string cmd;
+            string txt;
+
+            while (true)
+            {
+                if (!m_Tcp.Connected)
+                {
+                    m_State = ClientState.Disconnected;
+                    break;
+                }
+
+                line = reader.ReadLine();
+                cmd = line.Split(new char[] { ' ' })[0];
+                txt = line.Substring(cmd.Length);
+
+                switch (cmd)
+                {
+                    case "!message":
+                        if (txt.Length > 0) 
+                            SendTextToAll(txt);
+                        break;
+                    case "!getclientlist":
+                        break;
+                    case "!getfilelist":
+                        break;
+                    case "!getfreefileserver":
+                        break;
+                    case "!getfileserver":
+                        break;
+                }
+            }
         }
     }
 }
