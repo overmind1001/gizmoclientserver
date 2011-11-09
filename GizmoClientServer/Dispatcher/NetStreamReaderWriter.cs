13d23a21de6a8b0037d44c0f11011005456db10c
overmind1001 <overmind1001@277a856a-4f4b-458e-875c-85b963333f67>
2011-11-09 09:50:02 +0000

    Dispatcher. Создал класс для двунаправленного настроенного сетевого потока.

---
+++ GizmoClientServer/Dispatcher/NetStreamReaderWriter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Dispatcher
+{
+    class NetStreamReaderWriter
+    {
+        private StreamReader sr;
+        private StreamWriter sw;
+
+        public bool AutoFlush 
+        {
+            get 
+            {
+                return sw.AutoFlush;
+            }
+            set 
+            {
+                sw.AutoFlush = value;
+            }
+        }
+        public int ReadTimeout
+        {
+            get
+            {
+                return sr.BaseStream.ReadTimeout;
+            }
+            set
+            {
+                sr.BaseStream.ReadTimeout = value;
+            }
+        }
+
+        public NetStreamReaderWriter(Stream s)
+        {
+            sr = new StreamReader(s);
+            sw = new StreamWriter(s);
+            AutoFlush = true;
+            ReadTimeout = 10000;
+        }
+
+        public void WriteLine(string line)
+        {
+            sw.WriteLine(line);
+        }
+        public string ReadLine()
+        {
+            return sr.ReadLine();
+        }
+    }
+}
