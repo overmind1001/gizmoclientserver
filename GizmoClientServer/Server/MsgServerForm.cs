2c2cd5b3082cca12ce946ddab7bd22c3cff6d8e4
San4oKiselev@gmail.com <San4oKiselev@gmail.com@277a856a-4f4b-458e-875c-85b963333f67>
2011-11-18 19:23:05 +0000

    Положено начало переделки под новую архитектуру. "Сканер" свободных портов на сервере.

--- GizmoClientServer/Server/MsgServerForm.cs
+++ GizmoClientServer/Server/MsgServerForm.cs
@@ -11,120 +11,22 @@ using System.Net.Sockets;
 using System.Threading;
 using System.IO;
 using System.IO.Pipes;
+using System.Diagnostics;
 
 namespace MsgServer
 {
     public partial class MsgServerForm : Form
     {
-        // Поля
+        private TcpListener             m_Listener;                 // Tcp слушатель
+        private int                     m_DispatcherPort;           // Порт диспетчера
+        private int                     m_ServerPort;               // Порта сервера
+        private IPAddress               m_ServerIP;                 // IP сервера
+        private List<ServerItem>        m_ServersList;              // Список серверов
+        private List<ClientItem>        m_ClientList;               // Список клиентов
+        private Thread                  m_MainListenThread;         // Главный цикл прослушки порта
 
-        private TcpListener listener;                               // Прослушивает порт сервера для обнаружения новых tcp подключений
-
-        private int dispatcerPort;                                  // Номер порта диспетчера
-        private int serverPort;                                     // Номер порта сервера
-        private IPAddress serverIp;                                 // ip сервера
-
-        private List<ServerItem> serversList;                       // Список известных серверов
-        private List<ClientItem> сlientList;                        // Список клиентов
-
-        private ServerState state;                                  // Состояние сервера
-        private enum ServerState { Stopped, Running, Started };
-
-        private Thread mainMsgThread;                               // Поток, который прослушивает порт сервера и фиксирует новые подключения
-        
-        private bool isIsolated;                                    // Показывает, запущен ли сервер под управлением диспетчера или автономно
-        
-
-
-        // Методы
-
-        /// <summary>
-        /// Начальная инициализация 
-        /// </summary>
-        private void Init()
-        {
-            dispatcerPort   = 501;
-            serverPort      = 505;
-            serverIp        = Dns.Resolve("localhost").AddressList[0];
-
-            isIsolated      = false;
-
-            serversList     = new List<ServerItem>();
-            сlientList      = new List<ClientItem>();
-
-
-            listener        = new TcpListener(serverIp, serverPort);
-
-            state           = ServerState.Started;
-        }
-
-
-
-        /// <summary>
-        /// Подключиться к диспетчеру
-        /// </summary>
-        /// <returns>удачно ли</returns>
-        private bool ConnectToDispatcher()
-        {
-            MessageBox.Show("Не удалось подключиться к диспетчеру.\n" +
-                            "Сервер работает в автономном режиме.\n" +
-                            "Порт сервера: " + serverPort.ToString());
-            return false;
-        }
-
-
-
-        /// <summary>
-        /// Отключиться от диспетчера
-        /// </summary>
-        private void DisconnectToDispatcher()
-        {
-        }
-
-
-
-        /// <summary>
-        /// Запустить сервер
-        /// </summary>
-        private void StartServer()
-        {
-            if (state == ServerState.Running)
-                return;
-
-            state = ServerState.Running;
-
-            isIsolated = !ConnectToDispatcher();
-
-            mainMsgThread = new Thread(MainMsgFunc);
-            mainMsgThread.Start();
-
-            lblStatus.Text = isIsolated ? "автономно" : "подключен";
-        }
-
-        /// <summary>
-        /// Отключить сервер
-        /// </summary>
-        private void StopServer()
-        {
-            if (state == ServerState.Stopped)
-                return;
-
-            state = ServerState.Stopped;
-
-            isIsolated = true;
-            DisconnectToDispatcher();
-
-            if (mainMsgThread != null)
-                mainMsgThread.Abort();
-
-            listener.Stop();
-
-            lstClients.Items.Clear();
-            lstServers.Items.Clear();
-
-            lblStatus.Text = "отключен";
-        }
-
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Запуск и настройка сервера
 
 
         /// <summary>
@@ -133,166 +35,290 @@ namespace MsgServer
         public MsgServerForm()
         {
             InitializeComponent();
-
             Init();
         }
 
 
 
         /// <summary>
-        /// Добавить нового клиента
+        /// Проводит начальную инициализацию
         /// </summary>
-        /// <param name="tcp">tcp клиента</param>
-        private void AddClient(TcpClient tcp)
+        private void Init()
         {
-            ClientItem client = new ClientItem(tcp);
-            lock (сlientList)
-            {
-                сlientList.Add(client);
+            Debug.AutoFlush         = true;//для отладки
 
-                // Подписываемся на события клиента
-                client.SendTextToAll        += SendTextToAll;
-                client.GetClientList        += GetClientList;
-                client.GetFileList          += GetFileList;
-                client.GetFileServer        += GetFileServer;
-                client.GetFreeFileServer    += GetFreeFileServer;
-                client.IsContaind           += IsContained;
-                client.WriteLog             += WriteLog;
+            m_ServerIP              = Dns.GetHostEntry("localhost").AddressList[0];
+            m_ServerPort            = GetFreeListenerPort(m_Listener, m_ServerIP);
 
-                client.StartServe();
-            }
+            m_ServersList           = new List<ServerItem>();
+            m_ClientList            = new List<ClientItem>();
+
+            m_MainListenThread      = new Thread(MainListenThreadFunc);
         }
 
-        /// <summary>
-        /// Прослушивает порт на наличие новых подключений
-        /// </summary>
-        private void MainMsgFunc()
-        {
-            listener.Start();
-            while (true)
-            {
-                TcpClient tcp = listener.AcceptTcpClient();
-                AddClient(tcp);
-            }
-        }
+
 
         /// <summary>
-        /// Послать текстовое сообщение всем клиентам
+        /// Получает свободный порт и создаем для него tcp-слушаетеля
         /// </summary>
-        /// <param name="name">имя отправителя</param>
-        /// <param name="text">текст сообщения</param>
-        /// <returns></returns>
-        private bool SendTextToAll(string name, string text)
+        /// <param name="Listener">создаваемый слушаетль tcp</param>
+        /// <param name="IPAddr">ip адрес хоста</param>
+        /// <returns>номер порта</returns>
+        private int GetFreeListenerPort(TcpListener Listener, IPAddress IPAddr)
         {
-            lock (сlientList)
+            int     StartPort       = 49160;
+            int     EndPort         = 65534;
+            int     RandPort        = 0;
+            bool    HasFreePort     = false;
+            Random  Rand            = new Random();
+            
+            while(!HasFreePort)
             {
-                for (int i = 0; i < сlientList.Count; i++)
+                try
                 {
-                    сlientList[i].SendText(name, text);
+                    RandPort = Rand.Next(StartPort, EndPort);
+                    Listener = new TcpListener(IPAddr, RandPort);
+
+                    Listener.Start();
+                    Listener.Stop();
+
+                    HasFreePort = true;
+
+                    Debug.WriteLine(" > Server. Свободный порт найден! Порт: " + RandPort);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(" > Server. Порт занят: " + RandPort);
                 }
             }
-            WriteLog(" > " + name + ": " + text);
-            return true;
+
+            return RandPort;
         }
+        
+
 
         /// <summary>
-        /// Возвращает список клиентов
+        /// Главный цикл tcp-слушателя. Работает в отдельном потоке
         /// </summary>
-        /// <returns></returns>
-        private string GetClientList()
+        private void MainListenThreadFunc()
         {
-            string List = "";
-            lock (сlientList)
-            {
-                for (int i = 0; i < сlientList.Count; i++)
-                {
-                    if (!сlientList[i].IsRegister())
-                        continue;
 
-                    if (List != "") 
-                        List += "|";
-                    List += сlientList[i].GetName();
-                }
-            }
-            return List;
-        }
-
-        /// <summary>
-        /// Возвращает список файлов
-        /// </summary>
-        /// <returns>список файлов</returns>
-        private string GetFileList()
-        {
-            return "";
-        }
-
-        /// <summary>
-        /// Возвращет имя сервера, на котором содержится файл
-        /// </summary>
-        /// <returns>имя сервера</returns>
-        private string GetFileServer(string filename)
-        {
-            return "";
         }
 
 
         /// <summary>
-        /// Возвращает имя свободного файлового сервера
+        /// Запускает сервер
         /// </summary>
-        /// <returns></returns>
-        private string GetFreeFileServer()
+        private void StartServer()
         {
-            return "";
         }
 
         /// <summary>
-        /// Проверяет на наличие в списке клиентов клиента, зарегистрированного по данному имени
+        /// Останавливает работу сервера
         /// </summary>
-        /// <param name="name">проверяемое имя</param>
-        /// <returns>true - клиент с таким именем уже зарегистрирован</returns>
-        private bool IsContained(string name)
+        private void StopServer()
         {
-            lock (сlientList)
-            {
-                for (int i = 0; i < сlientList.Count; i++)
-                {
-                    if (сlientList[i].GetName() == name && сlientList[i].IsRegister())
-                        return true;
-                }
-            }
-            return false;
         }
 
 
-        private void WriteLog(string text)
-        {
-            lock (lstLog)
-            {
-                lstLog.Items.Add(text);
-            }
-        }
+        ///// <summary>
+        ///// Подключиться к диспетчеру
+        ///// </summary>
+        ///// <returns>удачно ли</returns>
+        //private bool ConnectToDispatcher()
+        //{
+        //    MessageBox.Show("Не удалось подключиться к диспетчеру.\n" +
+        //                    "Сервер работает в автономном режиме.\n" +
+        //                    "Порт сервера: " + serverPort.ToString());
+        //    return false;
+        //}
 
-        //////////////////////////////////////////////////////////////////////
-        // Обработчики событий формы
 
-        private void поднятьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            StartServer();
-        }
 
-        private void уронитьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            StopServer();
-        }
+        ///// <summary>
+        ///// Отключиться от диспетчера
+        ///// </summary>
+        //private void DisconnectToDispatcher()
+        //{
+        //}
 
-        private void MsgServerForm_Load(object sender, EventArgs e)
-        {
-            StartServer();
-        }
 
-        private void MsgServerForm_FormClosed(object sender, FormClosedEventArgs e)
-        {
-            StopServer();
-        }
+
+        ///// <summary>
+        ///// Запустить сервер
+        ///// </summary>
+        //private void StartServer()
+        //{
+        //    if (state == ServerState.Running)
+        //        return;
+
+        //    state = ServerState.Running;
+
+        //    isIsolated = !ConnectToDispatcher();
+
+        //    mainMsgThread = new Thread(MainMsgFunc);
+        //    mainMsgThread.Start();
+
+        //    lblStatus.Text = isIsolated ? "автономно" : "подключен";
+        //}
+
+        ///// <summary>
+        ///// Отключить сервер
+        ///// </summary>
+        //private void StopServer()
+        //{
+        //    if (state == ServerState.Stopped)
+        //        return;
+
+        //    state = ServerState.Stopped;
+
+        //    isIsolated = true;
+        //    DisconnectToDispatcher();
+
+        //    if (mainMsgThread != null)
+        //        mainMsgThread.Abort();
+
+        //    listener.Stop();
+
+        //    lstClients.Items.Clear();
+        //    lstServers.Items.Clear();
+
+        //    lblStatus.Text = "отключен";
+        //}
+
+
+
+        ///// <summary>
+        ///// Добавить нового клиента
+        ///// </summary>
+        ///// <param name="tcp">tcp клиента</param>
+        //private void AddClient(TcpClient tcp)
+        //{
+        //    ClientItem client = new ClientItem(tcp);
+        //    lock (сlientList)
+        //    {
+        //        сlientList.Add(client);
+
+        //        // Подписываемся на события клиента
+        //        client.SendTextToAll        += SendTextToAll;
+        //        client.GetClientList        += GetClientList;
+        //        client.GetFileList          += GetFileList;
+        //        client.GetFileServer        += GetFileServer;
+        //        client.GetFreeFileServer    += GetFreeFileServer;
+        //        client.IsContaind           += IsContained;
+        //        client.WriteLog             += WriteLog;
+
+        //        client.StartServe();
+        //    }
+        //}
+
+        ///// <summary>
+        ///// Прослушивает порт на наличие новых подключений
+        ///// </summary>
+        //private void MainMsgFunc()
+        //{
+        //    listener.Start();
+        //    while (true)
+        //    {
+        //        TcpClient tcp = listener.AcceptTcpClient();
+        //        AddClient(tcp);
+        //    }
+        //}
+
+        ///// <summary>
+        ///// Послать текстовое сообщение всем клиентам
+        ///// </summary>
+        ///// <param name="name">имя отправителя</param>
+        ///// <param name="text">текст сообщения</param>
+        ///// <returns></returns>
+        //private bool SendTextToAll(string name, string text)
+        //{
+        //    lock (сlientList)
+        //    {
+        //        for (int i = 0; i < сlientList.Count; i++)
+        //        {
+        //            сlientList[i].SendText(name, text);
+        //        }
+        //    }
+        //    WriteLog(" > " + name + ": " + text);
+        //    return true;
+        //}
+
+        ///// <summary>
+        ///// Возвращает список клиентов
+        ///// </summary>
+        ///// <returns></returns>
+        //private string GetClientList()
+        //{
+        //    string List = "";
+        //    lock (сlientList)
+        //    {
+        //        for (int i = 0; i < сlientList.Count; i++)
+        //        {
+        //            if (!сlientList[i].IsRegister())
+        //                continue;
+
+        //            if (List != "") 
+        //                List += "|";
+        //            List += сlientList[i].GetName();
+        //        }
+        //    }
+        //    return List;
+        //}
+
+        ///// <summary>
+        ///// Возвращает список файлов
+        ///// </summary>
+        ///// <returns>список файлов</returns>
+        //private string GetFileList()
+        //{
+        //    return "";
+        //}
+
+        ///// <summary>
+        ///// Возвращет имя сервера, на котором содержится файл
+        ///// </summary>
+        ///// <returns>имя сервера</returns>
+        //private string GetFileServer(string filename)
+        //{
+        //    return "";
+        //}
+
+
+        ///// <summary>
+        ///// Возвращает имя свободного файлового сервера
+        ///// </summary>
+        ///// <returns></returns>
+        //private string GetFreeFileServer()
+        //{
+        //    return "";
+        //}
+
+        ///// <summary>
+        ///// Проверяет на наличие в списке клиентов клиента, зарегистрированного по данному имени
+        ///// </summary>
+        ///// <param name="name">проверяемое имя</param>
+        ///// <returns>true - клиент с таким именем уже зарегистрирован</returns>
+        //private bool IsContained(string name)
+        //{
+        //    lock (сlientList)
+        //    {
+        //        for (int i = 0; i < сlientList.Count; i++)
+        //        {
+        //            if (сlientList[i].GetName() == name && сlientList[i].IsRegister())
+        //                return true;
+        //        }
+        //    }
+        //    return false;
+        //}
+
+
+        //private void WriteLog(string text)
+        //{
+        //    lock (lstLog)
+        //    {
+        //        lstLog.Items.Add(text);
+        //    }
+        //}
     }
 }
