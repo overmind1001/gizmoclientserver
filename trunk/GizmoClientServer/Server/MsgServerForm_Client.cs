ad8127a910b11a4c53c61cc2b6db0b748aa992fa
San4oKiselev@gmail.com <San4oKiselev@gmail.com@277a856a-4f4b-458e-875c-85b963333f67>
2011-11-20 06:41:00 +0000

    Разбил группы методов по файлам, для удобства

---
+++ trunk/GizmoClientServer/Server/MsgServerForm_Client.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System.IO.Pipes;
+using System.Diagnostics;
+using Dispatcher;
+
+namespace MsgServer
+{
+    public partial class MsgServerForm : Form
+    {
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Взаимодействие с клиентом
+
+        /// <summary>
+        /// Генирирует команду ответа на запрос "!who"
+        /// </summary>
+        /// <returns>команда</returns>
+        private NetCommand AnsWho()
+        {
+            return CreateCommand("!messageserver", "Сервер сообщений");
+        }
+
+        /// <summary>
+        /// Регистрирует клиента и генерирует команду ответа
+        /// </summary>
+        /// <param name="RegComand">команда запроса регистрации</param>
+        /// <returns>команда</returns>
+        private NetCommand AnsRegister(NetCommand RegCmd)
+        {
+            NetCommand RetCmd;
+
+            // Если сервер переполнен
+            if (GetClientCount() >= m_MaxClientCount)
+                RetCmd = CreateCommand("!unregistred", "Сервер переполнен");
+
+            // Если клиент с таким именем уже имеется
+            else if (FindClient(RegCmd.sender) != null)
+            {
+                RetCmd = CreateCommand("!unregistred", "Клиент с таким именем уже зарегистрирован");
+            }
+
+            // Если все норм, регистрируем
+            else if (AddClient(RegCmd.sender, RegCmd.Ip, RegCmd.Port))
+                RetCmd = CreateCommand("!registred", "Регистрация успешно завершена");
+
+            // Если по какой то причине регистрация не удалась
+            else
+                RetCmd = CreateCommand("!unregistred", "Регистрация не удалась");
+
+            return RetCmd;
+        }
+
+        /// <summary>
+        /// Генерирует команду в ответ на запрос списка клиентов
+        /// </summary>
+        /// <returns>команда</returns>
+        private NetCommand AnsClientList()
+        {
+            return CreateCommand("!clientlist", GetClientList());
+        }
+
+        /// <summary>
+        /// Генерирует команду в ответ на запрос списка файлов
+        /// </summary>
+        /// <returns>команда</returns>
+        private NetCommand AnsFileList()
+        {
+            return CreateCommand("!filelist", "");
+        }
+
+        /// <summary>
+        /// Генерирует команду в ответ на запрос свободного файлового сервера
+        /// </summary>
+        /// <returns>команда ответа</returns>
+        private NetCommand AnsFreeFileServer()
+        {
+            return CreateCommand("!null", "");
+        }
+
+        /// <summary>
+        /// Генерирует команду в ответ на запрос файлового сервера на котором расположен файл
+        /// </summary>
+        /// <param name="filename">имя файла</param>
+        /// <returns>команда ответа</returns>
+        private NetCommand AnsFileServer(string filename)
+        {
+            return CreateCommand("!null", "");
+        }
+
+        /// <summary>
+        /// Обработчик клиентской команды
+        /// </summary>
+        /// <param name="Stream">читатель-писатель</param>
+        /// <param name="Cmd">команда</param>
+        private void ClientCommandHandler(NetStreamReaderWriter Stream, NetCommand Cmd)
+        {
+            switch (Cmd.cmd)
+            {
+                // Инициализация, или "кому я пишу?"
+                case "!who":
+                    {
+                        Stream.WriteCmd(AnsWho());
+                    }
+                    break;
+
+                // Регистрация клиента
+                case "!register":
+                    {
+                        Stream.WriteCmd(AnsRegister(Cmd));
+                    }
+                    break;
+
+                // Сообщение всем клиентам
+                case "!message":
+                    {
+                        SendMsgToAllClients(Cmd.sender, Cmd.parameters);
+                        Stream.WriteCmd(CreateCommand("!ok", "Вас понял"));
+                    }
+                    break;
+
+                // Запрос списка контактов
+                case "!getclientlist":
+                    {
+                        Stream.WriteCmd(AnsClientList());
+                    }
+                    break;
+
+                // Запрос списка файлов
+                case "!getfilelist":
+                    {
+                        Stream.WriteCmd(AnsFileList());
+                    }
+                    break;
+
+                // Запрос свободного файлового сервера для закачки
+                case "!getfreefileserver":
+                    {
+                        Stream.WriteCmd(AnsFreeFileServer());
+                    }
+                    break;
+
+                // Запрос файлового сервера для скачки
+                case "!getfileserver":
+                    {
+                        Stream.WriteCmd(AnsFileServer(Cmd.parameters));
+                    }
+                    break;
+
+                // Неизвестная команда
+                default:
+                    {
+                        Stream.WriteCmd(CreateCommand("!unknowncommand",
+                            "К сожалению сервер не знает такой команды"));
+                        UiWriteLog("Такая команда неизвестна серверу!");
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ trunk/GizmoClientServer/Server/MsgServerForm_ListenTcp.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System.IO.Pipes;
+using System.Diagnostics;
+using Dispatcher;
+
+
+
+namespace MsgServer
+{
+    public partial class MsgServerForm : Form
+    {
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Методы отвечающие за прослушку TCP
+
+        /// <summary>
+        /// Цикл прослушки новых tcp-соединений от клиентов. Работает в отдельном потоке
+        /// </summary>
+        private void TcpListenThreadFunc()
+        {
+            try
+            {
+                while (true)
+                {
+                    TcpClient Tcp = m_Listener.AcceptTcpClient();
+                    ThreadPool.QueueUserWorkItem(TcpThreadFunc, Tcp);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(" > Server. Ошибка в ClientsListenThreadFunc: " + ex.Message);
+            }
+        }
+
+
+
+        /// <summary>
+        /// Реализует реакцию на команду из tcp-соединения
+        /// </summary>
+        /// <param name="Tcp">tcp-отправителя</param>
+        private void TcpThreadFunc(object tcp)
+        {
+            TcpClient Tcp = (TcpClient)tcp;
+
+            try
+            {
+                NetStreamReaderWriter Stream = new NetStreamReaderWriter(Tcp.GetStream());
+                NetCommand Cmd = Stream.ReadCmd();
+
+                UiWriteLog("От " + Cmd.sender + " поступила команда '" + Cmd.cmd + "'");
+
+                // Если отправитель - другой сервер сообщений
+                if (Cmd.sender == "msgserver")
+                {
+                }
+
+                // Если отправитель - диспетчер
+                else if (Cmd.sender == "dispatcher")
+                {
+                }
+
+                // Если отправитель - клиент
+                else
+                {
+                    ClientCommandHandler(Stream, Cmd);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(" > Server. Ошибка в TcpThreadFunc: " + ex.Message);
+            }
+        }
+
+
+
+
+    }
+}
+
